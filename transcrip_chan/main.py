32e53b593d933b9098b32adf5844a0adbe99537d
sashida <sashidapc373443@gmail.com>
2024-02-19 06:08:16 +0900

    Change whisper lib to faster_whisper.

--- transcrip_chan/main.py
+++ transcrip_chan/main.py
@@ -1,18 +1,51 @@
 import os
 import wx
 from ui.user_interface import AppBaseFrame
-import whisper
+from faster_whisper import WhisperModel
 import logging
+import torch
+
+# global settings
+out_dir = "./文字起こし"
+model_dir = "./models"
+
+
+def init_logger(out_dir, logger_name, log_file_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_format = logging.Formatter('%(message)s')
+    console_handler.setFormatter(console_format)
+
+    file_handler = logging.FileHandler(
+        filename=os.path.join(out_dir, log_file_name))
+    file_format = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_format)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    return logger
+
+
+logger = init_logger(out_dir, 'transcrip_chan_logger', 'transcrip_chan.log')
+
+
+def get_device_settings() -> tuple[str, str]:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    torch_dtype = "float16" if torch.cuda.is_available() else "int8"
+    return device, torch_dtype
 
 
 def format_segment(segment):
-    start = segment["start"]
+    start = segment.start
     start_hour, start_min, start_sec, start_msec = map(
         int, (start / 3600, (start % 3600) / 60, start % 60, (start % 1) * 1000))
-    end = segment["end"]
+    end = segment.end
     end_hour, end_min, end_sec, end_msec = map(
         int, (end / 3600, (end % 3600) / 60, end % 60, (end % 1) * 1000))
-    text = segment["text"]
+    text = segment.text
     return f"{start_hour:02d}:{start_min:02d}:{start_sec:02d}.{start_msec:03d} --> {end_hour:02d}:{end_min:02d}:{end_sec:02d}.{end_msec:03d}\n{text}\n"
 
 
@@ -27,9 +60,8 @@ class AppFrame(AppBaseFrame):
         if len(selected_files) == 0:
             wx.MessageBox(
                 "文字起こしするファイルが選択されていません。ファイルをドロップしてください。", "エラー", wx.OK | wx.ICON_ERROR)
+            logger.exception(e)
             return
-
-        out_dir = "./文字起こし"
 
         try:
             if not os.path.exists(out_dir):
@@ -37,51 +69,66 @@ class AppFrame(AppBaseFrame):
         except Exception as e:
             wx.MessageBox(
                 f"出力先フォルダの作成に失敗しました。\r\n{e}", "エラー", wx.OK | wx.ICON_ERROR)
+            logger.exception(e)
             return
 
-        # initialize logging, output path = transcrip_chan.log
-        logging.basicConfig(filename=os.path.join(out_dir, "transcrip_chan.log"),
-                            level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        try:
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            model_name = self.get_selected_model()
+
+            logger.info(
+                f"\nAI モデルの読み込み中... model_name: {model_name}\n※初回のみダウンロードに時間がかかります。")
+            self.update_drop_text(
+                f"AI モデルの読み込み中...\r\n※初回のみダウンロードに時間がかかります。")
+
+            device, torch_dtype = get_device_settings()
+
+            model = WhisperModel(
+                model_name, device=device, compute_type=torch_dtype, download_root=model_dir)
+
+            logger.info(
+                f"AI モデルの読み込み完了: {model_name}, device: {device}, dtype: {torch_dtype}")
+        except Exception as e:
+            wx.MessageBox(
+                f"AI モデルの読み込みに失敗しました。\r\n{e}", "エラー", wx.OK | wx.ICON_ERROR)
+            logger.exception(e)
+            return
 
         error_count = 0
         for idx, selected_file in enumerate(selected_files):
-            logging.info(
-                f"selected_file: {selected_file} ({idx+1}/{len(selected_files)})")
+            logger.info(
+                f"'{selected_file}' の文字起こしを開始します。 ({idx+1}/{len(selected_files)})")
 
             try:
-                if not os.path.exists("./models"):
-                    os.makedirs("./models")
-                model_name = self.get_selected_model()
                 filename = os.path.basename(selected_file)
                 filename_wo_ext = os.path.splitext(filename)[0]
 
                 self.update_drop_text(
                     f"{filename} を文字起こし中...({len(selected_files)} 個中 {idx+1} 個目)\r\n時間がかかりますのでしばらくお待ち下さい。 \r\n（「応答なし⌛」になっても処理は進んでいるので気長にどうぞ）")
-                model = whisper.load_model(
-                    name=model_name, download_root="./models")
 
-                logging.info(
-                    f"model: {model}\nselected_file: {selected_file}\nstart transcribe...")
-                result = whisper.transcribe(model=model, audio=selected_file)
+                logger.info(
+                    f"model: {model_name}\nselected_file: {selected_file}\nstart transcribe...")
+                segments, info = model.transcribe(selected_file, beam_size=5)
 
-                segments = result["segments"]
+                logger.info("Detected language '%s' with probability %f" %
+                            (info.language, info.language_probability))
 
                 transcribe_text = "\n".join(format_segment(segment)
                                             for segment in segments)
-                logging.info(f"transcribe_text: {transcribe_text}")
 
                 out_file_path = os.path.join(
                     out_dir, f"文字起こし_{filename_wo_ext}.txt")
 
                 with open(out_file_path, "w", encoding="utf-8") as f:
                     f.write(transcribe_text)
-                logging.info(f"Transcription saved to {out_file_path}")
+                logger.info(f"Transcription saved to {out_file_path}")
             except Exception as e:
                 out_file_path = os.path.join(
                     out_dir, f"文字起こし_{filename_wo_ext}.txt")
                 with open(out_file_path, "w", encoding="utf-8") as f:
                     f.write(f"文字起こし中にエラーが発生しました。\n\nエラー詳細:\n{e}")
-                logging.exception(e)
+                logger.exception(e)
                 error_count += 1
 
         self.selected_files = []
@@ -92,6 +139,9 @@ class AppFrame(AppBaseFrame):
         if error_count == 0:
             wx.MessageBox(
                 f"文字起こしが完了しました。\r\n結果は [{out_dir}] に保存されました。", "文字起こし完了", wx.OK | wx.ICON_INFORMATION)
+        elif error_count == len(selected_files):
+            wx.MessageBox(
+                f"文字起こしに失敗しました。\r\nエラー詳細は [{out_dir}] の各ファイルをご確認ください。", "文字起こし失敗", wx.OK | wx.ICON_ERROR)
         else:
             wx.MessageBox(
                 f"文字起こしが完了しましたが、{error_count} 個のファイルでエラーが発生しました。\r\nエラー詳細は [{out_dir}] の各ファイルをご確認ください。", "文字起こし完了", wx.OK | wx.ICON_WARNING)
@@ -112,7 +162,7 @@ class AppFrame(AppBaseFrame):
         elif model_lebel == "中":
             return "medium"
         elif model_lebel == "大":
-            return "large"
+            return "large-v3"
         else:
             return "small"
 
